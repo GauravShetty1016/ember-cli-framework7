1ddfbea17d8c0aea732b3129fee15f28de3c3520
Gaurav Shetty <1016.gaurav@gmail.com>
2017-03-13 22:35:50 +0530

    Removing the tagname and using correct element

--- app/components/f7-search-bar.js
+++ app/components/f7-search-bar.js
@@ -15,7 +15,7 @@ export default Ember.Component.extend({
     if (searchList.length > 1) {
       throw new Error('There is more then one search list available within the search component.');
     }
-    this.get('f7.f7').initSearchbar(this.$('.searchlist'));
+    this.get('f7.f7').initSearchbar(this.$('.searchbar'));
   },
 
   onQueryChanged: function() {
