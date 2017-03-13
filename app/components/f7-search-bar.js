37f4e8e8c90ee32c1119b13809d958573c6b750b
Gaurav Shetty <1016.gaurav@gmail.com>
2017-03-13 22:48:04 +0530

    No more references to this.$

--- app/components/f7-search-bar.js
+++ app/components/f7-search-bar.js
@@ -18,7 +18,7 @@ export default Ember.Component.extend({
     if (searchList.length > 1) {
       throw new Error('There is more then one search list available within the search component.');
     }
-    this.get('f7.f7').initSearchbar(this.$('.searchbar'));
+    this.get('f7.f7').initSearchbar('.searchbar');
   },
 
   onQueryChanged: function() {
